9e53c71a424ac2209f3c93e4b3a604510695b86b
unknown <eduardo@escribo.com>
2022-05-10 23:46:17 -0300

    created film_list_page to test

--- token_mdb/lib/src/presenter/pages/film_list_page.dart
+++ token_mdb/lib/src/presenter/pages/film_list_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:token_mdb/src/presenter/presenter.dart';
 
+import '../../domain/domain.dart';
+
 class FilmListPage extends StatefulWidget {
   const FilmListPage({Key? key}) : super(key: key);
 
@@ -21,12 +23,67 @@ class _FilmListPageState extends ModularState<FilmListPage, FilmListStore> {
             store.getFilms();
             return const CircularProgressIndicator();
           } else {
-            return Container(
-              color: Colors.blue,
-            );
+            return ListView.builder(
+                itemCount: store.filmList.length,
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    onTap: () {},
+                    child: CardWidget(size: size, movie: store.filmList[index]),
+                  );
+                });
           }
         }),
         height: size.height,
+      ),
+    );
+  }
+}
+
+class CardWidget extends StatelessWidget {
+  const CardWidget({Key? key, required this.size, required this.movie})
+      : super(key: key);
+  final Size size;
+  final MovieGeneralInformation movie;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: size.height * 0.35,
+      width: size.width,
+      padding: const EdgeInsets.only(
+        left: 16,
+        right: 16,
+        top: 8,
+        bottom: 8,
+      ),
+      child: Card(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              height: size.height * 0.2,
+              child: Image.network(
+                movie.posterUrl,
+                fit: BoxFit.fill,
+              ),
+            ),
+            SizedBox(
+              width: size.width * 0.65,
+              child: Text(
+                movie.title,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+                style: const TextStyle(
+                  fontSize: 25,
+                ),
+              ),
+            ),
+            // Padding(
+            //   padding: const EdgeInsets.only(right: 20.0, bottom: 20),
+            //   child: Icon(icon),
+            // ),
+          ],
+        ),
       ),
     );
   }
